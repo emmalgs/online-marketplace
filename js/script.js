f82e71ee1e54248e8723562bc13abe5cbeea7738
Sarah Reimann <reimannsarah45@gmail.com>
2023-03-23 16:15:46 -0700

    add functions for price, discount, and final price
    	co-authored-by: Emma Gerigscott <elgerig@gmail.com>
    dquote> co-authored-by: Raed Omar <raed.alkhanbashi@gmail.com>

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ function userInput (event) {
     const cvv = document.querySelector ("#cvv").value;
     const zipcode = document.querySelector ("#zipcode").value;
     const shipping = document.querySelector ("#shipping").value;
+    document.getElementById("receipt").removeAttribute("class");
     receipt (whiteboard, name, creditcard, cvv, shipping, zipcode);
 }
 
@@ -16,22 +17,42 @@ function receipt (whiteboard, name, creditcard, cvv, shipping, zipcode) {
     const shipaddress = document.getElementById("shipaddress");
     const discount = document.getElementById("discount");
     const finalprice = document.getElementById("finalprice");
-    const thanks = document.getElementById("thanks")
+    const thanks = document.getElementById("thanks");
+    const wb1 = parseFloat(document.getElementById("wb1").dataset.cost);
+    const wb2 = parseFloat(document.getElementById("wb2").dataset.cost);
+    const wb3 = parseFloat(document.getElementById("wb3").dataset.cost);
+    const wb4 = parseFloat(document.getElementById("wb4").dataset.cost);
 
-    thanks.innerText = `Thank you, ${name}`
+    thanks.innerText = `Thank you, ${name}`;
 
     ccnumber.innerText = creditcard;
     shipaddress.innerText = shipping + zipcode;
     
-    if (whiteboard.value === '1') {
+    if (whiteboard === '1') {
         boardtype.innerText = "Baby's First Whiteboard";
-        price.innerText = whiteboard.dataset.cost;
-
+        price.innerText = `$ ${wb1}`;
+        discount.innerText = "60%"
+        finalprice.innerText = `$ ${discounted(wb1)}`;
+    } else if (whiteboard=== '2') {
+        boardtype.innerText = "Rainbow Board";
+        price.innerText = `$ ${wb2}`;
+        discount.innerText = "60%"
+        finalprice.innerText = `$ ${discounted(wb2)}`;
+    } else if (whiteboard=== "3") {
+        boardtype.innerText = "Big Boy Board";
+        price.innerText = `$ ${wb3}`;
+        discount.innerText = "60%"
+        finalprice.innerText = `$ ${discounted(wb3)}`;
+    } else if (whiteboard === '4') {
+        boardtype.innerText = "Cadillac Board";
+        price.innerText = `$ ${wb4}`;
+        discount.innerText = "60%"
+        finalprice.innerText = `$ ${discounted(wb4)}`;
     }
 } 
 
-function getPrice() {
-    return 
+function discounted(cost) {
+    return (cost * .4).toFixed(2)
 }
 
 
